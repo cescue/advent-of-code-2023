2dc80131b2c23307dec8e74ef3284e52783a4ffa
chris escue <chris@escue.io>
2023-12-06 07:32:18 -0600

    Improve boat calculations with the power of high school math

--- scripts/6b.rb
+++ scripts/6b.rb
@@ -5,30 +5,7 @@ input = File.readlines('6_input.txt')
 time = input.first.gsub(/[^0-9]/, '').to_i
 record = input.last.gsub(/[^0-9]/, '').to_i
 
-ways_to_win = []
+min = (-time - Math.sqrt((time**2) - (4 * record))) / 2
+max = (-time + Math.sqrt((time**2) - (4 * record))) / 2
 
-midpoint = time / 2
-ms_offset = 0
-
-while true
-  next_ms = midpoint + ms_offset
-  prev_ms = midpoint - ms_offset
-
-  next_speed = next_ms
-  next_travel_time = time - next_ms
-  next_distance = next_speed * next_travel_time
-
-  ways_to_win << next_ms if next_distance > record
-
-  prev_speed = prev_ms
-  prev_travel_time = time - prev_ms
-  prev_distance = prev_speed * prev_travel_time
-
-  ways_to_win << prev_ms if prev_distance > record
-
-  break if prev_distance < record && next_distance < record
-
-  ms_offset += 1
-end
-
-puts ways_to_win.uniq.length
+puts (max - min).ceil
